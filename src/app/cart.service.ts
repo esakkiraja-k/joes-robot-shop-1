c7e03f370311e11586ad0c50f84867df31e1b91c
esakkiraja-k <EsakkiRaja@KANINI-LTP-1122>
2024-08-01 15:00:15 +0530

    Added services

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,16 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
   cart: IProduct[] = [];
   
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   add(product: IProduct){
     this.cart.push(product);
+    
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+      console.log(`added ${product.name} to cart!!`);
+    });
+
+
     console.log(`product ${product.name} is added to cart`);
   }
 
